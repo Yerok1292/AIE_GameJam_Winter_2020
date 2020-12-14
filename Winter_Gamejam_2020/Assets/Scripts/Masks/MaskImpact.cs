e80bb54584111539f185db74f41c7399ea3dc893
kevincbryan <kevincbryan@gmail.com>
2020-12-14 15:54:33 -0800

    Added Mask conversion scripts

---
+++ Winter_Gamejam_2020/Assets/Scripts/Masks/MaskImpact.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskImpact : MonoBehaviour
+{
+  private WearingMask cMask;
+  private Collider cCollider;
+
+    private void OnCollisionEnter(Collision other) 
+    {
+        cCollider = other.collider;
+        cMask = cCollider.GetComponent<WearingMask>();
+
+        Debug.Log (cMask);
+
+        if (cMask)
+        {
+            cMask.Mask();
+            Destroy(gameObject, .5f);
+        }
+    }
+
+}
---
+++ Winter_Gamejam_2020/Assets/Scripts/Masks/WearingMask.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WearingMask : MonoBehaviour
+{
+
+    public bool masked = false;
+    // Start is called before the first frame update
+   
+
+   public void Mask ()
+   {
+       masked = true;
+   }
+
+   public bool isMasked ()
+   {
+       return (masked);
+   }
+}
