7e267115280a7301b1334f9c2c0b3e24cff037e8
kevincbryan <kevincbryan@gmail.com>
2020-12-14 15:19:00 -0800

    Added Aiming and Throwing of Masks

--- Winter_Gamejam_2020/Assets/Scripts/Player Controller/MaskTracker.cs
+++ Winter_Gamejam_2020/Assets/Scripts/Player Controller/MaskTracker.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class MaskTracker : MonoBehaviour
 {
+
+    public int maxMask = 5;
+    private int minMask = 0;
+    public int publicMask = 5; //Display value for masks
+    private int currentMask;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (maxMask < 1) maxMask = 1;
+
+        currentMask = maxMask;
+        publicMask = currentMask;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   public bool HasMask ()
+   {
+       if (currentMask > 0) return (true);
+       else return (false);
+   }
+
+   public bool Use ()
+   {
+       if (currentMask >= 1)
+       {
+           currentMask--;
+           publicMask = currentMask;
+           return(true);
+       }
+       else
+       {
+           return(false);
+       }
+   }
+
+   public void Refresh ()
+   {
+       currentMask = maxMask;
+       publicMask = currentMask;
+   }
+
+
 }
---
+++ Winter_Gamejam_2020/Assets/Scripts/Player Controller/ThrowMask.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowMask : MonoBehaviour
+{
+
+    public GameObject maskProjectile;
+    private GameObject tempMask;
+    private Rigidbody rbPackage;
+    public float throwForce = 25f;
+    private PlayerMove mPlayer;
+    private bool fireNow = false;
+    public AudioSource mailThrow;
+    public Transform launchPosition;
+
+    public MaskTracker mMaskTracker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mPlayer = gameObject.GetComponent<PlayerMove>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Fire1"))
+        {
+            //Debug.Log ("Fire1 is up");
+            bool hasAmmo = mMaskTracker.HasMask();
+            //Debug.Log (hasAmmo);
+
+            if (hasAmmo == true)
+            {
+                fireNow = true;
+                //Debug.Log("Should now fire");
+            } 
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        if (fireNow == true)
+        {
+            //Debug.Log("Mask should be thrown");
+            // AudioManager.PlaySound();
+            fireNow = false;
+            mMaskTracker.Use();
+
+           
+                if (maskProjectile)
+                {
+
+                    tempMask = Instantiate(maskProjectile, launchPosition);
+
+                    rbPackage = tempMask.GetComponent<Rigidbody>();
+
+                    if (rbPackage)
+                    {
+                        //rbPackage.isKinematic = false;
+                        rbPackage.AddForce(transform.forward * throwForce, ForceMode.Impulse);
+                    }
+
+
+
+
+                }
+            
+        }
+    }
+}
