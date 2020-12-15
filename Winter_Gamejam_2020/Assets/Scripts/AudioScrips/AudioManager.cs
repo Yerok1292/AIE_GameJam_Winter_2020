c9079c6c500230513a1f169d5e6c2e8629d684ae
Cat Levine <50648323+Yerok1292@users.noreply.github.com>
2020-12-15 12:29:25 -0800

    cat
    audio assets added

---
+++ Winter_Gamejam_2020/Assets/Scripts/AudioScrips/AudioManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+
+    public string name;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(.1f, 3f)]
+    public float pitch;
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+
+    }
+
+    void Start()
+    {
+        // Play("Music");
+        // Play("PunkYell");
+
+    }
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        s.source.Play();
+    }
+}
+
+// Add to scrip to add sound: AudioManager.instance.Play("NAMEOFSOUND");
