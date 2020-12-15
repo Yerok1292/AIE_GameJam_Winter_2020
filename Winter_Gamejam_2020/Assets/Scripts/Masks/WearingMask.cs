144266fda74dd3423a559a641783eda95ea4ac2e
kevincbryan <kevincbryan@gmail.com>
2020-12-15 10:52:12 -0800

    Integrated Sickness VFX with Customer Prefab

--- Winter_Gamejam_2020/Assets/Scripts/Masks/WearingMask.cs
+++ Winter_Gamejam_2020/Assets/Scripts/Masks/WearingMask.cs
@@ -1,17 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class WearingMask : MonoBehaviour
 {
 
     public bool masked = false;
+    public VisualEffect sickness;
     // Start is called before the first frame update
    
+   private void Start() 
+   {
+       if (masked == true)
+       {
+           if (sickness)
+           {
+               sickness.Stop();
+           }
+       }
+   }
+
+
 
    public void Mask ()
    {
        masked = true;
+
+        if (sickness)
+        {
+            sickness.Stop();
+        }
+   }
+
+   public void UnMask ()
+   {
+       masked = false;
+        if (sickness)
+        {
+            sickness.Stop();
+        }
    }
 
    public bool isMasked ()
