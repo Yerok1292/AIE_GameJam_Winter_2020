613d26907200f6e302bef694c4bd5039f4486137
Devonlg98 <54076403+Devonlg98@users.noreply.github.com>
2020-12-16 05:34:09 -0800

    Devon G. Nearly finished with health bar
    Repair*/clean system:
    % Chance for item to break when AI approaches
    Item breaking
    Sanitation DOT with short delay
    Player interacting/fixing
    All done,
    
    need to get done:
    Healthbar UI updates score based on tiers, 75% 50% 25% etc.
    Coworker who sits at stand, sick customers will hurt their health

--- Winter_Gamejam_2020/Assets/Scripts/BreakRepairItem.cs
+++ Winter_Gamejam_2020/Assets/Scripts/BreakRepairItem.cs
@@ -4,21 +4,76 @@ using UnityEngine;
 
 public class BreakRepairItem : MonoBehaviour
 {
+
+    public float dotTimerMax = 3;
+    public float dotTimerCurrent;
+    public float dotTickRate = 1;
+
+    //Dont edit
+    [HideInInspector]
+    public float dotTickRateCurrent;
+
+    public float sanitizationDamage = 1;
+
     public bool broken = false;
 
     [Range(1, 100)]
     public float BreakingChance = 50;
 
+    //Healthbar functionallity mostly from Brackeys
+    //www.youtube.com/watch?v=BLfNP4Sc_iA
+    public float maxHealth = 100;
+    public float currentHealth;
+    public HealthBar healthBar;
+
+    void Start()
+    {
+        dotTickRateCurrent = 0;
+        dotTimerCurrent = 0;
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+
+    }
+
+    void Update()
+    {
+
+        // If machine is broken for longer than dottimer
+        // damage the sanitization healthbar, everytime the tickrate hits
+        // its max, ie every one second take damage.
+
+        //Might need to update later to check for bars current health
+        // before updating incase multiple broken items save
+        // separate bars
+        // fixed ^ keeping note for future reference
+        if (broken == true)
+        {
+            dotTimerCurrent += Time.deltaTime;
+            dotTickRateCurrent += Time.deltaTime;
+        }
+
+        if(dotTimerCurrent >= dotTimerMax && dotTickRateCurrent >=dotTickRate)
+        {
+            currentHealth = healthBar.currentHealth;
+            currentHealth -= sanitizationDamage;
+            dotTickRateCurrent = 0;
+            healthBar.SetHealth(currentHealth);
+        }
+    }
+
     void OnTriggerEnter(Collider collision)
     {
         Debug.Log("Touched Machine");
+        FindObjectOfType<AudioManager>().Play("broken slushee machine");
+
         //First childed model = fixed
         GameObject repairedObject = transform.GetChild(0).gameObject;
         //Second childed model = broken
         GameObject brokenObject = transform.GetChild(1).gameObject;
         if (collision.tag == "Customer" && broken == false)
         {
-            //Play audio for using 
+            //Play audio for using
+
             Debug.Log("Used Machine");
 
             if (Random.value <= BreakingChance / 100)
@@ -35,6 +90,7 @@ public class BreakRepairItem : MonoBehaviour
 
     public void repairItem()
     {
+        FindObjectOfType<AudioManager>().Stop("broken slushee machine");
         //First childed model = fixed
         GameObject repairedObject = transform.GetChild(0).gameObject;
         //Second childed model = broken
